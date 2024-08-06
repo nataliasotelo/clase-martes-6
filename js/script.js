3d0221261efe17c48d37387ea84a7a236b658699
Natalia <natisotelo20@gmail.com>
2024-08-06 10:41:01 -0300

    cargar datos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const DATA = [
+  { name: "Laura", lastname: "Hightower" },
+  { name: "Roberto", lastname: "Carlos" },
+  { name: "Fulano", lastname: "Detal" },
+  { name: "Joaquina", lastname: "Hand" },
+  { name: "Maria", lastname: "White" },
+];
+
+// Escribe el código necesario aquí
+
+//
